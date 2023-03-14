f8c73315941226b2d0e620b840cb13597dbb6ec7
Nico <nmanograsso@gmail.com>
2023-03-14 17:48:09 -0300

    add: dependecies

--- Package.swift
+++ Package.swift
@@ -5,22 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "PokedexData",
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PokedexData",
             targets: ["PokedexData"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(path: "../PokedexDomain"),
+        .package(path: "../SwiftNetworking")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PokedexData",
-            dependencies: []),
+            dependencies: ["PokedexDomain", "SwiftNetworking"]),
         .testTarget(
             name: "PokedexDataTests",
             dependencies: ["PokedexData"]),
